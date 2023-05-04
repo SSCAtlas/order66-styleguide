d238ed3486bc1ea2ff3e6e4343a2b0608c409aad
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2023-05-04 15:47:17 -0700

    Test pagination

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import LabeledDropdown from './components/LabeledDropdown.vue'
+import Pagination from './components/Pagination'
 export default {
 	install: (app, options) => {
 		app.component("LabeledDropdown", LabeledDropdown)
+		app.component("Pagination", Pagination)
 	}
 }
