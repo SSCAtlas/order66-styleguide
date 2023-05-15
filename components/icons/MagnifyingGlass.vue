a6884cf1f8295b07e8675ff7770a97a2dcf10966
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2023-05-15 16:34:07 -0700

    Just because you can doesnt mean you should

--- components/icons/MagnifyingGlass.vue
+++ components/icons/MagnifyingGlass.vue
@@ -16,7 +16,6 @@
 
 <script>
 export default {
-	name: 'magglass',
 	props: {
 		fillColor: {
 			type: String,
--- index.js
+++ index.js
@@ -1,157 +1,152 @@
-// import LabeledDropdown from './components/LabeledDropdown.vue'
-// import Pagination from './components/Pagination.vue'
-// import AccordionArrowDown from './components/icons/AccordionArrowDown.vue'
-// import ArrowCircleLeft from './components/icons/ArrowCircleLeft.vue'
-// import ArrowCircleRight from './components/icons/ArrowCircleRight.vue'
-// import DropdownCaret from './components/icons/DropdownCaret.vue'
-// import ActionArrow from './components/icons/ActionArrow.vue'
-// import ArchedArrow from './components/icons/ArchedArrow.vue'
-// import Bell from './components/icons/Bell.vue'
-// import Calendar from './components/icons/Calendar.vue'
-// import CalendarPlus from './components/icons/CalendarPlus.vue'
-// import CancelX from './components/icons/CancelX.vue'
-// import Checklist from './components/icons/Checklist.vue'
-// import Checkmark from './components/icons/Checkmark.vue'
-// import CircleExclamation from './components/icons/CircleExclamation.vue'
-// import CircleX from './components/icons/CircleX.vue'
-// import Clipboard from './components/icons/Clipboard.vue'
-// import Clock from './components/icons/Clock.vue'
-// import CloseX from './components/icons/CloseX.vue'
-// import CollapseLeft from './components/icons/CollapseLeft.vue'
-// import CollapseRight from './components/icons/CollapseRight.vue'
-// import CompassArrow from './components/icons/CompassArrow.vue'
-// import CrossEye from './components/icons/CrossEye.vue'
-// import Directory from './components/icons/Directory.vue'
-// import Discussion from './components/icons/Discussion.vue'
-// import DownArrow from './components/icons/DownArrow.vue'
-// import DownloadCloud from './components/icons/DownloadCloud.vue'
-// import Edit from './components/icons/Edit.vue'
-// import EditReport from './components/icons/EditReport.vue'
-// import Email from './components/icons/Email.vue'
-// import EmptyCheckbox from './components/icons/EmptyCheckbox.vue'
-// import EventsCalendar from './components/icons/EventsCalendar.vue'
-// import EventsList from './components/icons/EventsList.vue'
-// import EventsReport from './components/icons/EventsReport.vue'
-// import EvntLogo from './components/icons/EvntLogo.vue'
-// import Excel from './components/icons/Excel.vue'
-// import ExclamationPoint from './components/icons/ExclamationPoint.vue'
-// import ExpandLeft from './components/icons/ExpandLeft.vue'
-// import ExpandRight from './components/icons/ExpandRight.vue'
-// import Files from './components/icons/Files.vue'
-// import Filters from './components/icons/Filters.vue'
-// import FourSquares from './components/icons/FourSquares.vue'
-// import Hamburger from './components/icons/Hamburger.vue'
-// import LeftArrow from './components/icons/LeftArrow.vue'
-// import MagnifyingGlass from './components/icons/MagnifyingGlass.vue'
-// import Microphone from './components/icons/Microphone.vue'
-// import Notification from './components/icons/Notification.vue'
-// import Overview from './components/icons/Overview.vue'
-// import PaperClip from './components/icons/PaperClip.vue'
-// import PaperList from './components/icons/PaperList.vue'
-// import PersonCheck from './components/icons/PersonCheck.vue'
-// import PersonDvvisits from './components/icons/PersonDvvisits.vue'
-// import PersonEngage from './components/icons/PersonEngage.vue'
-// import PersonEngagements from './components/icons/PersonEngagements.vue'
-// import PersonExternal from './components/icons/PersonExternal.vue'
-// import PersonInternal from './components/icons/PersonInternal.vue'
-// import PersonList from './components/icons/PersonList.vue'
-// import PersonPlus from './components/icons/PersonPlus.vue'
-// import Phone from './components/icons/Phone.vue'
-// import PieChart from './components/icons/PieChart.vue'
-// import Plus from './components/icons/Plus.vue'
-// import ReplyBubble from './components/icons/ReplyBubble.vue'
-// import RightArrow from './components/icons/RightArrow.vue'
-// import RoundedSquare from './components/icons/RoundedSquare.vue'
-// import Sort from './components/icons/Sort.vue'
-// import ThickArrowUp from './components/icons/ThickArrowUp.vue'
-// import Timeline from './components/icons/Timeline.vue'
-// import TrashCanX from './components/icons/TrashCanX.vue'
-// import Trello from './components/icons/Trello.vue'
-// import User18 from './components/icons/User18.vue'
-// import User19 from './components/icons/User19.vue'
-// import User20 from './components/icons/User20.vue'
-// import User27 from './components/icons/User27.vue'
+import LabeledDropdown from './components/LabeledDropdown.vue'
+import Pagination from './components/Pagination.vue'
+import AccordionArrowDown from './components/icons/AccordionArrowDown.vue'
+import ArrowCircleLeft from './components/icons/ArrowCircleLeft.vue'
+import ArrowCircleRight from './components/icons/ArrowCircleRight.vue'
+import DropdownCaret from './components/icons/DropdownCaret.vue'
+import ActionArrow from './components/icons/ActionArrow.vue'
+import ArchedArrow from './components/icons/ArchedArrow.vue'
+import Bell from './components/icons/Bell.vue'
+import Calendar from './components/icons/Calendar.vue'
+import CalendarPlus from './components/icons/CalendarPlus.vue'
+import CancelX from './components/icons/CancelX.vue'
+import Checklist from './components/icons/Checklist.vue'
+import Checkmark from './components/icons/Checkmark.vue'
+import CircleExclamation from './components/icons/CircleExclamation.vue'
+import CircleX from './components/icons/CircleX.vue'
+import Clipboard from './components/icons/Clipboard.vue'
+import Clock from './components/icons/Clock.vue'
+import CloseX from './components/icons/CloseX.vue'
+import CollapseLeft from './components/icons/CollapseLeft.vue'
+import CollapseRight from './components/icons/CollapseRight.vue'
+import CompassArrow from './components/icons/CompassArrow.vue'
+import CrossEye from './components/icons/CrossEye.vue'
+import Directory from './components/icons/Directory.vue'
+import Discussion from './components/icons/Discussion.vue'
+import DownArrow from './components/icons/DownArrow.vue'
+import DownloadCloud from './components/icons/DownloadCloud.vue'
+import Edit from './components/icons/Edit.vue'
+import EditReport from './components/icons/EditReport.vue'
+import Email from './components/icons/Email.vue'
+import EmptyCheckbox from './components/icons/EmptyCheckbox.vue'
+import EventsCalendar from './components/icons/EventsCalendar.vue'
+import EventsList from './components/icons/EventsList.vue'
+import EventsReport from './components/icons/EventsReport.vue'
+import EvntLogo from './components/icons/EvntLogo.vue'
+import Excel from './components/icons/Excel.vue'
+import ExclamationPoint from './components/icons/ExclamationPoint.vue'
+import ExpandLeft from './components/icons/ExpandLeft.vue'
+import ExpandRight from './components/icons/ExpandRight.vue'
+import Files from './components/icons/Files.vue'
+import Filters from './components/icons/Filters.vue'
+import FourSquares from './components/icons/FourSquares.vue'
+import Hamburger from './components/icons/Hamburger.vue'
+import LeftArrow from './components/icons/LeftArrow.vue'
+import MagnifyingGlass from './components/icons/MagnifyingGlass.vue'
+import Microphone from './components/icons/Microphone.vue'
+import Notification from './components/icons/Notification.vue'
+import Overview from './components/icons/Overview.vue'
+import PaperClip from './components/icons/PaperClip.vue'
+import PaperList from './components/icons/PaperList.vue'
+import PersonCheck from './components/icons/PersonCheck.vue'
+import PersonDvvisits from './components/icons/PersonDvvisits.vue'
+import PersonEngage from './components/icons/PersonEngage.vue'
+import PersonEngagements from './components/icons/PersonEngagements.vue'
+import PersonExternal from './components/icons/PersonExternal.vue'
+import PersonInternal from './components/icons/PersonInternal.vue'
+import PersonList from './components/icons/PersonList.vue'
+import PersonPlus from './components/icons/PersonPlus.vue'
+import Phone from './components/icons/Phone.vue'
+import PieChart from './components/icons/PieChart.vue'
+import Plus from './components/icons/Plus.vue'
+import ReplyBubble from './components/icons/ReplyBubble.vue'
+import RightArrow from './components/icons/RightArrow.vue'
+import RoundedSquare from './components/icons/RoundedSquare.vue'
+import Sort from './components/icons/Sort.vue'
+import ThickArrowUp from './components/icons/ThickArrowUp.vue'
+import Timeline from './components/icons/Timeline.vue'
+import TrashCanX from './components/icons/TrashCanX.vue'
+import Trello from './components/icons/Trello.vue'
+import User18 from './components/icons/User18.vue'
+import User19 from './components/icons/User19.vue'
+import User20 from './components/icons/User20.vue'
+import User27 from './components/icons/User27.vue'
 import ListApi from './js/ListApi.js'
-import * as Icons from './components/icons'
 export default {
 	ListApi,
 	install: (app, options) => {
-		Icons.map(icon => {
-			console.log(app.component(`"${icon}"`, icon))
-		return app.component(`"${icon}"`, icon)
-		})
-// 		app.component("LabeledDropdown", LabeledDropdown)
-// 		app.component("Pagination", Pagination)
-// 		app.component("AccordionArrowDown", AccordionArrowDown)
-// 		app.component("ActionArrow", ActionArrow)
-// 		app.component("ArchedArrow", ArchedArrow)
-// 		app.component("Bell", Bell)
-// 		app.component("Calendar", Calendar)
-// 		app.component("CalendarPlus", CalendarPlus)
-// 		app.component("CancelX", CancelX)
-// 		app.component("Checklist", Checklist)
-// 		app.component("Checkmark", Checkmark)
-// 		app.component("CircleExclamation", CircleExclamation)
-// 		app.component("CircleX", CircleX)
-// 		app.component("Clipboard", Clipboard)
-// 		app.component("Clock", Clock)
-// 		app.component("CloseX", CloseX)
-// 		app.component("CollapseLeft", CollapseLeft)
-// 		app.component("CollapseRight", CollapseRight)
-// 		app.component("CompassArrow", CompassArrow)
-// 		app.component("CrossEye", CrossEye)
-// 		app.component("Directory", Directory)
-// 		app.component("Discussion", Discussion)
-// 		app.component("DownArrow", DownArrow)
-// 		app.component("DownloadCloud", DownloadCloud)
-// 		app.component("Edit", Edit)
-// 		app.component("EditReport", EditReport)
-// 		app.component("Email", Email)
-// 		app.component("EmptyCheckbox", EmptyCheckbox)
-// 		app.component("EventsCalendar", EventsCalendar)
-// 		app.component("EventsList", EventsList)
-// 		app.component("EventsReport", EventsReport)
-// 		app.component("EvntLogo", EvntLogo)
-// 		app.component("Excel", Excel)
-// 		app.component("ExclamationPoint", ExclamationPoint)
-// 		app.component("ExpandLeft", ExpandLeft)
-// 		app.component("ExpandRight", ExpandRight)
-// 		app.component("Files", Files)
-// 		app.component("Filters", Filters)
-// 		app.component("FourSquares", FourSquares)
-// 		app.component("Hamburger", Hamburger)
-// 		app.component("LeftArrow", LeftArrow)
-// 		app.component("MagnifyingGlass", MagnifyingGlass)
-// 		app.component("Microphone", Microphone)
-// 		app.component("Notification", Notification)
-// 		app.component("Overview", Overview)
-// 		app.component("PaperClip", PaperClip)
-// 		app.component("PaperList", PaperList)
-// 		app.component("PersonCheck", PersonCheck)
-// 		app.component("PersonDvvisits", PersonDvvisits)
-// 		app.component("PersonEngage", PersonEngage)
-// 		app.component("PersonEngagements", PersonEngagements)
-// 		app.component("PersonExternal", PersonExternal)
-// 		app.component("PersonInternal", PersonInternal)
-// 		app.component("PersonList", PersonList)
-// 		app.component("PersonPlus", PersonPlus)
-// 		app.component("Phone", Phone)
-// 		app.component("PieChart", PieChart)
-// 		app.component("Plus", Plus)
-// 		app.component("ReplyBubble", ReplyBubble)
-// 		app.component("RightArrow", RightArrow)
-// 		app.component("RoundedSquare", RoundedSquare)
-// 		app.component("Sort", Sort)
-// 		app.component("ThickArrowUp", ThickArrowUp)
-// 		app.component("Timeline", Timeline)
-// 		app.component("TrashCanX", TrashCanX)
-// 		app.component("Trello", Trello)
-// 		app.component("User18", User18)
-// 		app.component("User19", User19)
-// 		app.component("User20", User20)
-// 		app.component("User27", User27)
-// 		app.component("DropdownCaret", DropdownCaret)
-// 		app.component("ArrowCircleLeft", ArrowCircleLeft)
-// 		app.component("ArrowCircleRight", ArrowCircleRight)
+		app.component("LabeledDropdown", LabeledDropdown)
+		app.component("Pagination", Pagination)
+		app.component("AccordionArrowDown", AccordionArrowDown)
+		app.component("ActionArrow", ActionArrow)
+		app.component("ArchedArrow", ArchedArrow)
+		app.component("Bell", Bell)
+		app.component("Calendar", Calendar)
+		app.component("CalendarPlus", CalendarPlus)
+		app.component("CancelX", CancelX)
+		app.component("Checklist", Checklist)
+		app.component("Checkmark", Checkmark)
+		app.component("CircleExclamation", CircleExclamation)
+		app.component("CircleX", CircleX)
+		app.component("Clipboard", Clipboard)
+		app.component("Clock", Clock)
+		app.component("CloseX", CloseX)
+		app.component("CollapseLeft", CollapseLeft)
+		app.component("CollapseRight", CollapseRight)
+		app.component("CompassArrow", CompassArrow)
+		app.component("CrossEye", CrossEye)
+		app.component("Directory", Directory)
+		app.component("Discussion", Discussion)
+		app.component("DownArrow", DownArrow)
+		app.component("DownloadCloud", DownloadCloud)
+		app.component("Edit", Edit)
+		app.component("EditReport", EditReport)
+		app.component("Email", Email)
+		app.component("EmptyCheckbox", EmptyCheckbox)
+		app.component("EventsCalendar", EventsCalendar)
+		app.component("EventsList", EventsList)
+		app.component("EventsReport", EventsReport)
+		app.component("EvntLogo", EvntLogo)
+		app.component("Excel", Excel)
+		app.component("ExclamationPoint", ExclamationPoint)
+		app.component("ExpandLeft", ExpandLeft)
+		app.component("ExpandRight", ExpandRight)
+		app.component("Files", Files)
+		app.component("Filters", Filters)
+		app.component("FourSquares", FourSquares)
+		app.component("Hamburger", Hamburger)
+		app.component("LeftArrow", LeftArrow)
+		app.component("MagnifyingGlass", MagnifyingGlass)
+		app.component("Microphone", Microphone)
+		app.component("Notification", Notification)
+		app.component("Overview", Overview)
+		app.component("PaperClip", PaperClip)
+		app.component("PaperList", PaperList)
+		app.component("PersonCheck", PersonCheck)
+		app.component("PersonDvvisits", PersonDvvisits)
+		app.component("PersonEngage", PersonEngage)
+		app.component("PersonEngagements", PersonEngagements)
+		app.component("PersonExternal", PersonExternal)
+		app.component("PersonInternal", PersonInternal)
+		app.component("PersonList", PersonList)
+		app.component("PersonPlus", PersonPlus)
+		app.component("Phone", Phone)
+		app.component("PieChart", PieChart)
+		app.component("Plus", Plus)
+		app.component("ReplyBubble", ReplyBubble)
+		app.component("RightArrow", RightArrow)
+		app.component("RoundedSquare", RoundedSquare)
+		app.component("Sort", Sort)
+		app.component("ThickArrowUp", ThickArrowUp)
+		app.component("Timeline", Timeline)
+		app.component("TrashCanX", TrashCanX)
+		app.component("Trello", Trello)
+		app.component("User18", User18)
+		app.component("User19", User19)
+		app.component("User20", User20)
+		app.component("User27", User27)
+		app.component("DropdownCaret", DropdownCaret)
+		app.component("ArrowCircleLeft", ArrowCircleLeft)
+		app.component("ArrowCircleRight", ArrowCircleRight)
 	}
 }
