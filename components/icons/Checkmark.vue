4c474cb4d680746bc8ea82071f7dc4fd25fc7575
Gene <64043816+Gene-avatech@users.noreply.github.com>
2023-10-06 17:30:08 -0700

    Checkmark resizable

--- components/icons/Checkmark.vue
+++ components/icons/Checkmark.vue
@@ -1,7 +1,8 @@
 <template>
-	<svg width="12" height="10" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 10">
 		<path fill-rule="evenodd" clip-rule="evenodd" d="M11.774.713a.75.75 0 0 1 .013 1.061l-7.325 7.5a.75.75 0 0 1-1.109-.04L.178 5.485a.75.75 0 0 1 1.144-.969l2.643 3.121 6.748-6.91a.75.75 0 0 1 1.061-.013Z"
-			:fill="fillColor" :stroke="strokeColor"
+			:fill="fillColor"
+			:stroke="strokeColor"
 		/>
 	</svg>
 </template>
