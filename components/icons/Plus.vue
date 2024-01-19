bbd92ae88c70a5d761a0ef1361e4e28cb5a9a38b
Gene <64043816+Gene-avatech@users.noreply.github.com>
2024-01-19 14:16:05 -0800

    Fix Plus icon

--- components/icons/Plus.vue
+++ components/icons/Plus.vue
@@ -1,16 +1,8 @@
 <template>
-	<svg
-		width="18"
-		height="18"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<path
-			d="M9 2.25v13.5M2.25 9h13.5"
+	<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33 33">
+		<path fill-rule="evenodd" clip-rule="evenodd" d="M18.093 5.729a1.487 1.487 0 0 0-2.974 0v8.92H5.987a1.487 1.487 0 0 0 0 2.974h9.132v9.132a1.487 1.487 0 1 0 2.974 0v-9.132h8.92a1.487 1.487 0 1 0 0-2.973h-8.92V5.729Z"
+			:fill="fillColor"
 			:stroke="strokeColor"
-			stroke-width="2.5"
-			stroke-linecap="round"
-			stroke-linejoin="round"
 		/>
 	</svg>
 </template>
@@ -20,7 +12,7 @@ export default {
 	props: {
 		fillColor: {
 			type: String,
-			default: "#F1F2F2",
+			default: "transparent",
 		},
 		strokeColor: {
 			type: String,
